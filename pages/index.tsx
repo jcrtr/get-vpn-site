91dd8f64d38df5f4603380f6d2f4f8cb4a7cc87d
likiblack <likiblack@gmail.com>
2022-08-05 11:48:40 +0300

    Adedd chanel

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type {NextPage} from 'next'
 import Head from 'next/head'
 import IconHead from '../components/iconHead'
 import BotInfo from '../components/botInfo'
-import {FaInfoCircle} from "react-icons/fa";
+import {FaBullhorn, FaInfoCircle} from "react-icons/fa";
 
 const Home: NextPage = () => {
     return (
@@ -15,16 +15,24 @@ const Home: NextPage = () => {
 
             <main className={'min-h-screen lg:bg-gray-100 flex items-center justify-center'}>
                 <div className={'flex flex-col lg:bg-white lg:rounded-large lg:w-1/4 w-full lg:h-1/2 p-6'}>
-                    <IconHead />
+                    <IconHead/>
                     <h1 className="text-3xl font-bold p-8 text-center">
                         Доступ к cамому быстрому VPN!
                     </h1>
-                    <BotInfo />
-                    <div className={'flex items-center justify-center'}>
-                        <FaInfoCircle />
-                        <p className={'ml-2'}>По всем вопросам <a href={'mailto:get_vpn_sup@proton.me'} className={'text-blue-600'}>get_vpn_sup@proton.me</a></p>
+                    <BotInfo/>
+                    <div className={'flex items-center justify-center py-1'}>
+                        <FaInfoCircle/>
+                        <p className={'ml-2'}>По всем вопросам <a href={'mailto:get_vpn_sup@proton.me'}
+                                                                  className={'text-blue-600'}>get_vpn_sup@proton.me</a>
+                        </p>
                     </div>
-                    <a href="tg:@wg_vpn_bot">
+                    <div className={'flex items-center justify-center py-1'}>
+                        <FaBullhorn/>
+                        <p className={'ml-2'}>Новости проекта <a href={'https://t.me/vpn_get'}
+                                                                  className={'text-blue-600'}>🚀 GET VPN INFO 🚀</a>
+                        </p>
+                    </div>
+                    <a href="https://t.me/wg_vpn_bot">
                         <button className={'w-full py-4 bg-blue-600 rounded-large font-bold text-white mt-8'}>
                             Перейти в Телеграмм бот
                         </button>
