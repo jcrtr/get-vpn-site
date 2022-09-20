c9b6c72a4fd7b9e6a8317755a05a6a55a0df923b
jcrtr <36883870+jcrtr@users.noreply.github.com>
2022-09-20 13:18:57 +0000

    Deploying to gh-pages from @ jcrtr/get-vpn-site@848933abccac70653e6a767605a97cdb942a0fc0 🚀

--- _next/static/_0lfASh6U_SSbxP2zJVGM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b98bc7c3-fbba0b519fcd593e.js","static/chunks/1bfc9850-c75934ca833c4db8.js","static/chunks/pages/index-9e3e8783559b945e.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
